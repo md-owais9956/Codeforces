6d37fc556a5796317eccb8d531b7755be2a49286
MOHD OWAIS <md.owais111234@gmail.com>
2026-08-22 01:01:29 +0530

    Add 2185C. Shifted MEX

---
+++ 2185C. Shifted MEX/Solution.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int n;
+        cin >> n;
+
+        vector<long long> a(n);
+
+        for (auto &x : a)
+            cin >> x;
+
+        sort(a.begin(), a.end());
+
+        int ans = 1;
+        int curr = 1;
+
+        for (int i = 1; i < n; i++) {
+
+            if (a[i] == a[i - 1])
+                continue;
+
+            if (a[i] == a[i - 1] + 1)
+                curr++;
+            else
+                curr = 1;
+
+            ans = max(ans, curr);
+        }
+
+        cout << ans << '\n';
+    }
+
+    return 0;
+}
