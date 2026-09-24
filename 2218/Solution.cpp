3895d50e001be5692afc19bde217e09f8badc3e2
MOHD OWAIS <md.owais111234@gmail.com>
2026-09-24 23:35:01 +0530

    Add 2218E

---
+++ 2218/Solution.cpp
@@ -0,0 +1,34 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int n;
+        cin >> n;
+
+        vector<int> a(n);
+
+        for (int i = 0; i < n; i++) {
+            cin >> a[i];
+        }
+
+        int ans = 0;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                ans = max(ans, a[i] ^ a[j]);
+            }
+        }
+
+        cout << ans << '\n';
+    }
+
+    return 0;
+}
